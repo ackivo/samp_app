7b5ab7cd38f0e5304e7e514057b2b8895df3fc08
Canek <canek43@yahoo.com>
2011-02-15 20:35:21 -0500

    Unfix

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ SampApp::Application.routes.draw do
 
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
-  match '/help',    :to => 'pages#help'
-  match '/', :to => 'pages#home'
+  match '/help',    :to => 'pages#help'  
+  
+  root :to => 'pages#home'
   
   get "pages/home"
 
