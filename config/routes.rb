8376ac35eb8c5edfce6fe9cca3ad199b93666f1a
Canek <canek43@yahoo.com>
2011-02-15 20:17:07 -0500

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+
   root :to => 'pages#home'
 
   
