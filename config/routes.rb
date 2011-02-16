91044d63dc10b8f437f8f383a4673d389a920311
Canek <canek43@yahoo.com>
2011-02-15 20:26:29 -0500

    Attempt to fix home

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ SampApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/', :to => 'pages#home'
+
 
   root :to => 'pages#home'
 
